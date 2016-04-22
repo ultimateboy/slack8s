1bff4ae3c1ccbee56aaed4bd8b479a2334dfa04f
Matt Tucker <matt@newmediadenver.com>
2016-04-22 10:15:14 -0600

    Readme with example kube manifests, and minor updates.

---
+++ README.md
@@ -0,0 +1,30 @@
+# Kubernetes Events Slack Bot (slack8s)
+
+Infinite loop go program which queries the Kubernetes Event Stream API and
+posts messages to slack for important events.
+
+## Building
+
+Given this is an active work in progress, you'll probably want to modify
+and build the code yourself. If you just want to get started quickly,
+use the image in [docker hub](https://hub.docker.com/r/ultimateboy/slack8s/).
+
+1. `docker build .`
+2. Tag and push to your favorite registry
+
+## Running
+
+1. Copy the example configmap file:  
+`cp examples/example.slack8s-configmap.yaml slack8s-configmap.yaml`
+2. Modify `slack-token` and `slack-channel` variables in your new file.
+3. Create the config map using kubectl:  
+`kubectl create -f examples/slack8s-configmap.yaml`
+4. Create the slack8s replication controller:  
+`kubectl create -f examples/slack8s-rc.yaml`
+
+# Todo
+
+1. Refactor the way in which the types of alerts to send to slack are configured
+2. Add more types of alerts which get posted to slack
+3. Better formatting of slack attachment output
+4. Alerting thresholds
--- main.go
+++ main.go
@@ -4,17 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strings"
+	"time"
 
 	"github.com/nlopes/slack"
-)
-
-const (
-	KubeHost       = ""
-	SlackToken     = ""
-	SlackChannelID = ""
 )
 
 type Response struct {
@@ -28,6 +25,9 @@ type Event struct {
 	Metadata       EventMetadata       `json:"metadata"`
 	Reason         string              `json:"reason"`
 	Message        string              `json:"message"`
+	FirstTimestamp time.Time           `json:"firstTimestamp"`
+	LastTimestamp  time.Time           `json:"lastTimestamp"`
+	Count          int                 `json:"count"`
 }
 
 type EventMetadata struct {
@@ -43,12 +43,16 @@ type EventInvolvedObject struct {
 	Kind string `json:"kind"`
 }
 
-func send_message(e Event) error {
-	api := slack.New(SlackToken)
+func send_message(e Event, color string) error {
+	api := slack.New(os.Getenv("SLACK_TOKEN"))
 	params := slack.PostMessageParameters{}
 	attachment := slack.Attachment{
-		AuthorName: e.Message,
+		Fallback: e.Message,
 		Fields: []slack.AttachmentField{
+			slack.AttachmentField{
+				Title: "Message",
+				Value: e.Message,
+			},
 			slack.AttachmentField{
 				Title: "Object",
 				Value: e.InvolvedObject.Kind,
@@ -73,59 +77,102 @@ func send_message(e Event) error {
 	}
 
 	// Colors!
-	if strings.HasPrefix(e.Reason, "Successful") {
+	if color != "" {
+		attachment.Color = color
+	} else if strings.HasPrefix(e.Reason, "Success") {
 		attachment.Color = "good"
-	} else if strings.HasPrefix(e.Reason, "Failed") {
+	} else if strings.HasPrefix(e.Reason, "Fail") {
 		attachment.Color = "danger"
 	}
 	params.Attachments = []slack.Attachment{attachment}
 
-	channelID, timestamp, err := api.PostMessage(SlackChannelID, "", params)
+	channelID, timestamp, err := api.PostMessage(os.Getenv("SLACK_CHANNEL"), "", params)
 	if err != nil {
 		fmt.Printf("%s\n", err)
 		return err
 	}
 
-	fmt.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
+	log.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
 	return nil
 }
 
 func main() {
-	url := fmt.Sprintf("%s/api/v1/events?watch=true", KubeHost)
+	url := fmt.Sprintf("http://localhost:8001/api/v1/events?watch=true")
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Fatal("NewRequest: ", err)
-		return
 	}
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal("Do: ", err)
-		return
 	}
 	defer resp.Body.Close()
 	dec := json.NewDecoder(resp.Body)
+	if resp.StatusCode != 200 {
+		log.Printf(string(resp.Status) + ": " + string(resp.StatusCode))
+		log.Fatal("Non 200 status code returned from Kubernetes API.")
+	}
 	for {
 		var r Response
 		if err := dec.Decode(&r); err == io.EOF {
+			log.Printf("EOF detected.")
 			break
 		} else if err != nil {
-			log.Fatal(err)
+			htmlData, er := ioutil.ReadAll(resp.Body)
+			if er != nil {
+				log.Printf("Already errored, but failed to read response for log output.")
+			}
+			log.Printf(string(htmlData))
+			log.Fatal("Decode: ", err)
 		}
 		e := r.Object
+
 		// Log all events for now.
-		fmt.Printf("%s: %s\n", e.Reason, e.Message)
+		log.Printf("Reason: %s\nMessage: %s\nCount: %s\nTimestamp: %s\n\n", e.Reason, e.Message, e.Count, e.FirstTimestamp)
 
 		send := false
+		color := ""
 
 		// @todo refactor the configuration of which things to post.
 		if e.Reason == "SuccessfulCreate" {
 			send = true
+			color = "good"
+		} else if e.Reason == "NodeReady" {
+			send = true
+			color = "good"
+		} else if e.Reason == "NodeNotReady" {
+			send = true
+			color = "warning"
+		} else if e.Reason == "NodeOutOfDisk" {
+			send = true
+			color = "danger"
 		}
+
+		// For now, dont alert multiple times, except if it's a backoff
+		if e.Count > 1 {
+			send = false
+		}
+		if e.Reason == "BackOff" && e.Count == 3 {
+			send = true
+			color = "danger"
+		}
+
+		// Do not send any events that are more than 1 minute old.
+		// This assumes events are processed quickly (very likely)
+		// in exchange for not re-notifying of events after a crash
+		// or fresh start.
+		diff := time.Now().Sub(e.LastTimestamp)
+		diffMinutes := int(diff.Minutes())
+		if diffMinutes > 1 {
+			log.Printf("Supressed old message: %s", e.Message)
+			send = false
+		}
+
 		if send {
-			err = send_message(e)
+			err = send_message(e, color)
 			if err != nil {
-				log.Fatal(err)
+				log.Fatal("send_message: ", err)
 			}
 		}
 	}
